b22de47330a6a4b10a088681e678b355e4904c1c
Aariz Shroff <shroffaariz@gmail.com>
2025-11-23 22:03:45 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 
 print("Welcome to my Python program!")
 points = input("How many points do you want ? ")
+points = float(points)
+
+weekly_points = points * 7
+print(f"You are on track {weekly_points} hours this week.")
