5cbe03908e5e870ec56b0676b85670d4f66b3704
Aariz Shroff <shroffaariz@gmail.com>
2025-11-23 22:11:37 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-
+#Welcome
 print("Welcome to my Python program!")
+#input taken
 points = input("How many points did you earn today? ")
+#input to number if wrong
 try:
     points = float(points)
 except ValueError:
-    print("Please enter a valid number.")
-    exit()  # stops the program if input is invalid
+    print("Please enter a valid number.")#inform user
+    exit()  # stop program if wrong
 
-
+#calualte weekly
 weekly_points = points * 7
+#display
 print(f"You have gotten {weekly_points} points this week.")
