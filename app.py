e1b5f51c51ecaf1d8ad98d1dc0ac4c9b1caefea2
Aariz Shroff <shroffaariz@gmail.com>
2025-11-23 22:08:48 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
 print("Welcome to my Python program!")
-points = input("How many points do you want ? ")
-points = float(points)
+points = input("How many points did you earn today? ")
+try:
+    points = float(points)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()  # stops the program if input is invalid
+
 
 weekly_points = points * 7
-print(f"You are on track {weekly_points} hours this week.")
+print(f"You have gotten {weekly_points} points this week.")
